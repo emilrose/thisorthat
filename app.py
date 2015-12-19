93e4e3c14607980845d3c4ff0bcbb075974f99d8
Fiona Condon <frcondon@gmail.com>
2015-12-18 21:03:13 -0500

    Adding test code for file uploading

--- app.py
+++ app.py
@@ -1,9 +1,34 @@
-from flask import Flask
-app = Flask(__name__)
+import os
+from flask import Flask, request, redirect, url_for
+from werkzeug import secure_filename
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+UPLOAD_FOLDER = 'uploads'
+
+app = Flask(__name__)
+app.debug = True
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] == "txt"
+
+
+@app.route("/", methods=['GET', 'POST'])
+def upload_file():
+    if request.method == "POST":
+        file = file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return "success"
+    return '''
+        <!doctype html>
+        <title>Upload new File</title>
+        <h1>Upload new File</h1>
+        <form action="" method=post enctype=multipart/form-data>
+          <p><input type=file name=file>
+             <input type=submit value=Upload>
+        </form>
+    '''
 
 if __name__ == "__main__":
     app.run()
