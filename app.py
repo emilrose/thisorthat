ff4375db3a9bf3bea3909c1605e76a0a64cb647c
emilrose <emil.rose@gmail.com>
2015-12-27 17:18:38 -0500

    Got React interaction with API working.

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import os
-from flask import Flask, request, redirect, url_for, render_template
+from flask import Flask, request, redirect, url_for, render_template, jsonify
 from werkzeug import secure_filename
-
 import quiz
+
 
 UPLOAD_FOLDER = 'uploads'
 
@@ -24,6 +24,27 @@ def upload_file():
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return "success"
     return render_template("index.html")
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
+
+
+@app.route('/api', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
 
 
 if __name__ == "__main__":
